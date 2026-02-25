62c0da5ab1451295fe64bc5ffc6a7007013d9108
Preyaphon <mtpreyaphon0225@gmail.com>
2026-02-26 00:48:06 +0700

    connect

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:8000/api';
+const API_URL = 'http://localhost:8000';
 let currentUserId = localStorage.getItem('user_id');
 let currentUsername = localStorage.getItem('username');
 let currentTodos = [];
